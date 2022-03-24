c03f2e053bec0d8ef4220c585b4bdcb521940122
Greg Sutcliffe <gsutclif@redhat.com>
2022-03-24 13:41:33 +0000

    Fix typo in update script

--- bots/update_config.sh
+++ bots/update_config.sh
@@ -10,7 +10,7 @@ echo "Updating section_template.md..."
 wget -nv -O section_template.md https://raw.githubusercontent.com/ansible-community/ansible.im/main/bots/newsbot-section.md
 
 echo "Updating project_template.md..."
-wget -nv -O project_template.md https://raw.githubusercontent.com/ansible-community/ansible.im/main/bots/newsbot-section.md
+wget -nv -O project_template.md https://raw.githubusercontent.com/ansible-community/ansible.im/main/bots/newsbot-project.md
 
 echo "Updating update_config.sh..."
 wget -nv -O update_config.sh https://raw.githubusercontent.com/ansible-community/ansible.im/main/bots/update_config.sh
